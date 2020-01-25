fa8068796f47cef717a4c4e7897fb975e949f244
Abel Kalum <chumbabel@gmail.com>
2020-01-25 01:02:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,5 @@
 def square_array(array)
-   numbers = [1,2,3]
-   square_array.each(numbers)
+  new_array = []
+  array.each{|a| new_array.push(a*a)}
+  return new_array
 end
-
-def square_array(ary)
-  a = Array.new(ary.length)
-  ary.each_index { |i| a[i] = ary[i] * ary[i] }
-  a
-end
-
