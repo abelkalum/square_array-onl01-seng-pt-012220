2352dd33ec23e4896d4d6758bd1e070f63d40922
Abel Kalum <chumbabel@gmail.com>
2020-01-25 00:58:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,4 @@
 def square_array(array)
-  numbers = [1,2,3]
-  square_array.collect(numbers)
-  square_array.map(numbers)
-  square_array.inject(numbers)
-end
-
-def square_array(array)
    numbers = [1,2,3]
    square_array.each(numbers)
 end
@@ -19,4 +12,3 @@ def square_array(array)
    new_numbers = [9,10,16,25]
    square_array.each(new_numbers)
 end
-
